6f4271eec3589671f9ad7ba8a243c6830dd56372
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-24 19:53:34 +0600

    Create get menu and reviews data

--- index.js
+++ index.js
@@ -26,9 +26,15 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bristroDB").collection("menu");
+    const reviewCollection = client.db("bristroDB").collection("reviews");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
       res.send(result);
     });
     // Send a ping to confirm a successful connection
