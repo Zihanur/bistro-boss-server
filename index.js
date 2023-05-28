9299a77e25383cb0915fca55087a4ada3bd20115
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-28 22:46:44 +0600

    Added get item in the cart

--- index.js
+++ index.js
@@ -38,6 +38,16 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
+    // cart get and post
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
     app.post("/carts", async (req, res) => {
       const item = req.body;
