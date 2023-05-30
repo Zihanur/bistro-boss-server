258019c7338ece29732eed38722529997030b00e
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-30 16:37:03 +0600

    Get DB all users

--- index.js
+++ index.js
@@ -25,10 +25,29 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const userCollection = client.db("bristroDB").collection("user");
     const menuCollection = client.db("bristroDB").collection("menu");
     const reviewCollection = client.db("bristroDB").collection("reviews");
     const cartCollection = client.db("bristroDB").collection("carts");
 
+    //users
+    app.get('/users', async(req,res)=>{
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({message: 'user already exists'})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //menu and reviews
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -39,7 +58,7 @@ async function run() {
       res.send(result);
     });
     // cart get and post
-    app.get('/carts', async (req, res) => {
+    app.get("/carts", async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
@@ -51,17 +70,16 @@ async function run() {
 
     app.post("/carts", async (req, res) => {
       const item = req.body;
-      console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
 
-    app.delete('/carts/:id',async(req,res)=>{
+    app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
